69ea399cbd3878cf23490c829d6a3fd4f9ae8686
Elvar Olafsson <b0b4f3tt@gmail.com>
2019-04-14 08:25:06 +0000

    Added a DataHandler class
    Added a DataHandler class that will act as a bridge between the GUI code and the TSP algorithms.

---
+++ src/main/java/elvaro/DataHandler.java
@@ -0,0 +1,26 @@
+package elvaro;
+
+import java.util.ArrayList;
+
+public class DataHandler {
+    private static final DataHandler dHandler = new DataHandler();
+    private ArrayList<Point> points;
+
+    /**
+     * Private constructor
+     */
+    private DataHandler() {
+    }
+
+    /**
+     * Returns the single instance of the DataHandler
+     * @return DataHandler object
+     */
+    public static DataHandler getDataHandler() {
+        return dHandler;
+    }
+
+    public void addPoints(ArrayList<Point> points) {
+        this.points = points;
+    }
+}
